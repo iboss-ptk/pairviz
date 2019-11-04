2546c9362fb6cb037cb794ba09427a035bbfb30f
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-11-04 09:11:13 +0700

    Collect commits and transform into map

---
+++ lib/pairviz/git.ex
@@ -0,0 +1,22 @@
+defmodule Pairviz.Git do
+  def pull(repo) do
+    with {res, 0} <- System.cmd("git", ["pull", "--force"], cd: repo) do
+      {:ok, res}
+    else
+      {res, code} -> {:error, "exit code #{code}: #{res}"}
+      any -> {:error, "unknown error: #{any}"}
+    end
+  end
+
+  def log(repo) do
+    {commits, 0} = System.cmd("git", ["log", "--pretty=format:%ci | %s"], cd: repo)
+
+    commits
+    |> String.split("\n")
+    |> Enum.map(fn commit ->
+      [date | _] = String.split(commit)
+      [_ | message] = String.split(commit, " | ", parts: 2)
+      %{date: date, message: message}
+    end)
+  end
+end
--- lib/pairviz_web/controllers/page_controller.ex
+++ lib/pairviz_web/controllers/page_controller.ex
@@ -1,7 +1,28 @@
 defmodule PairvizWeb.PageController do
   use PairvizWeb, :controller
+  alias Pairviz.Git, as: Git
+  alias Date
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    # put all related repos into the same dir
+    repos = ["exrepa"]
+
+    commits =
+      repos
+      |> Enum.flat_map(fn repo ->
+        Git.pull(repo)
+        Git.log(repo)
+      end)
+      |> Enum.map(fn %{date: date, message: message} -> "[#{date}] #{message}" end)
+
+    # collect commits, group them by date
+    # group all of them again by date => {date, [commits]}
+    # extract person id => { date, [p1, p2] } and [p1, p2] == [p2, p1] then uniq by date
+    # decay
+
+    # note: need to think about how to test. -> prep repos
+    # deal with ssh later
+
+    render(conn, "index.html", commits: commits)
   end
 end
