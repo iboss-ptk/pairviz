9e3d76306239fa73382abdec3fca7754a30adaff
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-11-05 09:52:54 +0700

    Implement function for extracting pair from git message

--- lib/pairviz/pairing.ex
+++ lib/pairviz/pairing.ex
@@ -1,5 +1,24 @@
 defmodule Pairviz.Pairing do
   def extract_pair(commit_message, pattern, splitter) do
-    ["Pat", "Jin"]
+    Regex.named_captures(pattern, commit_message)
+    |> nil_to_map
+    |> Map.fetch("names")
+    |> map_error(fn _ -> "name can't be found" end) >>>
+      (&String.split(&1, splitter))
   end
+
+  # util, to be extracted
+  defp nil_to_map(nil), do: %{}
+  defp nil_to_map(a), do: a
+
+  defp {:ok, res} >>> f, do: {:ok, f.(res)}
+  defp {:error, err} >>> _, do: {:error, err}
+  defp :error >>> _, do: {:error, nil}
+
+  # defp map({:ok, res}, f), do: {:ok, f.(res)}
+  # defp map(a, _), do: a
+
+  defp map_error({:error, err}, f), do: {:error, f.(err)}
+  defp map_error(:error, f), do: {:error, f.(nil)}
+  defp map_error(a, _), do: a
 end
--- test/pairviz/pairing_test.exs
+++ test/pairviz/pairing_test.exs
@@ -2,11 +2,27 @@ defmodule Pairviz.PairingTest do
   use ExUnit.Case
   alias Pairviz.Pairing, as: Pairing
 
-  test "extract pair when it's a simple pairing" do
-    assert Pairing.extract_pair(
-             "#123 | Pat:Jin | Hello world",
-             ~r/^.*|(.*)|.*$/,
-             ":"
-           ) == ["Pat", "Jin"]
+  @pipe_around_name ~r/^.*\| (?<names>.*) \|.*$/
+  @bracket_around_name ~r/^.*\[ (?<names>.*) \].*$/
+
+  test "extract pair can config pattern that capture names and its splitter" do
+    assert Pairing.extract_pair("#123 | Pat:Jin | Hello world", @pipe_around_name, ":") ==
+             {:ok, ["Pat", "Jin"]}
+
+    assert Pairing.extract_pair("#123 | Nate,Jones | Hello world", @pipe_around_name, ",") ==
+             {:ok, ["Nate", "Jones"]}
+
+    assert Pairing.extract_pair("[ Pat:Jin ] Hello world", @bracket_around_name, ":") ==
+             {:ok, ["Pat", "Jin"]}
+  end
+
+  test "extract pair failed when no name found" do
+    assert Pairing.extract_pair("#123 Hello world", @pipe_around_name, ":") ==
+             {:error, "name can't be found"}
+  end
+
+  test "extract pair allow multiple splitters" do
+    assert Pairing.extract_pair("#123 | Nate&Jones | Hello world", @pipe_around_name, [":", "&"]) ==
+             {:ok, ["Nate", "Jones"]}
   end
 end
