5fa46a57f0594c86e05be890acd3c39fdc40e4fe
Supanat Pothivarakorn <supanat.ptk@gmail.com>
2019-11-06 08:40:45 +0700

    Extract multiple pairing situation

--- lib/pairviz/pairing.ex
+++ lib/pairviz/pairing.ex
@@ -1,22 +1,36 @@
 defmodule Pairviz.Pairing do
-  def extract_pair(commit_message, pattern, splitter) do
+  def extract_pairs(commit_message, pattern, splitter) do
     Regex.named_captures(pattern, commit_message)
     |> nil_to_map
     |> Map.fetch("names")
     |> map_error(fn _ -> "name can't be found" end) >>>
-      (&String.split(&1, splitter))
+      fn name_string ->
+        names =
+          name_string
+          |> String.split(splitter)
+          |> Enum.map(&String.trim/1)
+
+        case names do
+          [] -> []
+          [a] -> [[a, a]]
+          ns -> ns |> pairs
+        end
+      end
   end
 
   # util, to be extracted
   defp nil_to_map(nil), do: %{}
   defp nil_to_map(a), do: a
 
+  defp pairs([]), do: []
+  defp pairs([head | tail]), do: Enum.map(tail, &[head, &1]) ++ pairs(tail)
+
   defp {:ok, res} >>> f, do: {:ok, f.(res)}
   defp {:error, err} >>> _, do: {:error, err}
   defp :error >>> _, do: {:error, nil}
 
   # defp map({:ok, res}, f), do: {:ok, f.(res)}
-  # defp map(a, _), do: a
+  # defp map(a, _), do: a  
 
   defp map_error({:error, err}, f), do: {:error, f.(err)}
   defp map_error(:error, f), do: {:error, f.(nil)}
--- test/pairviz/pairing_test.exs
+++ test/pairviz/pairing_test.exs
@@ -6,23 +6,42 @@ defmodule Pairviz.PairingTest do
   @bracket_around_name ~r/^.*\[ (?<names>.*) \].*$/
 
   test "extract pair can config pattern that capture names and its splitter" do
-    assert Pairing.extract_pair("#123 | Pat:Jin | Hello world", @pipe_around_name, ":") ==
-             {:ok, ["Pat", "Jin"]}
+    assert Pairing.extract_pairs("#123 | Pat:Jin | Hello world", @pipe_around_name, ":") ==
+             {:ok, [["Pat", "Jin"]]}
 
-    assert Pairing.extract_pair("#123 | Nate,Jones | Hello world", @pipe_around_name, ",") ==
-             {:ok, ["Nate", "Jones"]}
+    assert Pairing.extract_pairs("#123 | Nate,Jones | Hello world", @pipe_around_name, ",") ==
+             {:ok, [["Nate", "Jones"]]}
 
-    assert Pairing.extract_pair("[ Pat:Jin ] Hello world", @bracket_around_name, ":") ==
-             {:ok, ["Pat", "Jin"]}
+    assert Pairing.extract_pairs("[ Pat:Jin ] Hello world", @bracket_around_name, ":") ==
+             {:ok, [["Pat", "Jin"]]}
+
+    assert Pairing.extract_pairs("[ Pat :  Jin ] Hello world", @bracket_around_name, ":") ==
+             {:ok, [["Pat", "Jin"]]}
+  end
+
+  test "extract self pairing" do
+    assert Pairing.extract_pairs("#123 | Dan | Hello world", @pipe_around_name, ":") ==
+             {:ok, [["Dan", "Dan"]]}
   end
 
   test "extract pair failed when no name found" do
-    assert Pairing.extract_pair("#123 Hello world", @pipe_around_name, ":") ==
+    assert Pairing.extract_pairs("#123 Hello world", @pipe_around_name, ":") ==
              {:error, "name can't be found"}
   end
 
   test "extract pair allow multiple splitters" do
-    assert Pairing.extract_pair("#123 | Nate&Jones | Hello world", @pipe_around_name, [":", "&"]) ==
-             {:ok, ["Nate", "Jones"]}
+    assert Pairing.extract_pairs("#123 | Nate&Jones | Hello world", @pipe_around_name, [":", "&"]) ==
+             {:ok, [["Nate", "Jones"]]}
+  end
+
+  test "extract multiple pairs" do
+    assert Pairing.extract_pairs("#123 | Nate&Jones&Thomas | Hello world", @pipe_around_name, "&") ==
+             {:ok, [["Nate", "Jones"], ["Nate", "Thomas"], ["Jones", "Thomas"]]}
+
+    assert Pairing.extract_pairs("#123 | Nate &Jones : Thomas | Hello world", @pipe_around_name, [
+             "&",
+             ":"
+           ]) ==
+             {:ok, [["Nate", "Jones"], ["Nate", "Thomas"], ["Jones", "Thomas"]]}
   end
 end
